a15f41ae7b4c5533ab6aa1e8ab44afa2e73f6f8b
Dhiraj kumar <dhirajk22410@gmail.com>
2025-02-13 10:58:32 +0530

    Update Cloudinary.js

--- server/utils/Cloudinary.js
+++ server/utils/Cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-exports.cloudinaryConnect = ()=>{
+exports.cloudinaryConnecter = ()=>{
     try {
             cloudinary.config({
                 cloud_name: process.env.CLOUD_NAME,
